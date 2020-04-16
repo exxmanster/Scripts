90c722ee6e1f9a996a23746924e1880a7eb6ad2a
exxman <exxmanster@gmail.com>
2020-04-16 14:59:38 +0300

    Cleanup a bit

--- ub_postinstall.sh
+++ ub_postinstall.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
+###Update 
 apt update -y && apt upgrade -y 
-
+###Docker Install
     apt-get update
 
     apt-get install \
@@ -15,5 +16,8 @@ add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-    
+
+###Istall docker-compose
    apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-compose 
+
+   sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
