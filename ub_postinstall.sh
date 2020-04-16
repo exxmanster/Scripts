5b92ee1a3c5939e5ff250eb9d198a85217ba46d6
exxman <exxmanster@gmail.com>
2020-04-16 14:48:04 +0300

    first one

--- ub_postinstall.sh
+++ ub_postinstall.sh
@@ -1,1 +1,19 @@
+#/bin/bash
 
+apt update -y && apt upgrade -y 
+
+    apt-get update
+
+    apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+    
+   apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-compose 
